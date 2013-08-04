df32a679c2f3e21aa294c90cfb193b5d8e62a708
Simon Walker <s.r.walker101@googlemail.com>
2013-08-04 14:14:13 +0100

    Adds spec for nights attribute

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -8,4 +8,8 @@ describe Action do
     it "should have a night id" do
         expect(Action.new).to have_attribute(:night_id)
     end
+
+    it "should belong to a night" do
+        expect(Action.new).to have_attribute(:nights)
+    end
 end
