23944c70a1779105329febe8eeee5f89d7a78cdf
Simon Walker <s.r.walker101@googlemail.com>
2013-08-04 14:52:38 +0100

    Adds spec for linking to the night

--- spec/features/actions_spec.rb
+++ spec/features/actions_spec.rb
@@ -2,13 +2,22 @@ require 'spec_helper'
 
 describe "Actions" do
   describe "GET /actions" do
-    it "should show a list of actions" do
+      before(:each) do
+          @date = 2.days.ago
+          n = Night.create(:start_date => @date)
+          n.actions.create(:action_id => 100)
+          n.save
+          visit actions_path
+      end
 
-        Action.create(:action_id => 100)
+      it "should show a list of actions" do
+          expect(page).to have_content '100'
+      end
 
-
-      visit actions_path
-      expect(page).to have_content '100'
-    end
+      it "should have a link for the corresponding night" do
+          expect(page).to have_content 'View night'
+          click_link 'View night'
+          expect(page).to have_content @date
+      end
   end
 end
