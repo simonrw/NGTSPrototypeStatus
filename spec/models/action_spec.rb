fe8f10aba181ba3dbe01ba577d03b4a1061f69fd
Simon Walker <s.r.walker101@googlemail.com>
2013-08-04 14:14:05 +0100

    Adds spec for night_id

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -4,4 +4,8 @@ describe Action do
     it "should have an action id" do
         expect(Action.new).to have_attribute(:action_id)
     end
+
+    it "should have a night id" do
+        expect(Action.new).to have_attribute(:night_id)
+    end
 end
