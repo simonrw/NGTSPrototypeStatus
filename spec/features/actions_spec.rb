fabbc546115dbbf3f5b3464893b99e168b4c575a
Simon Walker <s.r.walker101@googlemail.com>
2013-08-04 14:37:33 +0100

    Adds integration test for actions

--- spec/features/actions_spec.rb
+++ spec/features/actions_spec.rb
@@ -4,8 +4,8 @@ describe "Actions" do
   describe "GET /actions" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get actions_index_path
-      response.status.should be(200)
+      visit actions_path
+      expect(response.status).to be(200)
     end
   end
 end
