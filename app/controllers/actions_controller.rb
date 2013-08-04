604376cd04262c9e9d2d19fe98e6792f020ea48b
Simon Walker <s.r.walker101@googlemail.com>
2013-08-04 14:41:01 +0100

    Adds actions controller

---
+++ app/controllers/actions_controller.rb
@@ -0,0 +1,22 @@
+class ActionsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
