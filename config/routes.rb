f02b82baa950b578c5870f362de79ecb40403926
Simon Walker <s.r.walker101@googlemail.com>
2013-08-04 16:10:14 +0100

    Adds route for measurements

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ NGTSPrototypeStatus::Application.routes.draw do
 
     resources :nights
     resources :actions do
+        resources :measurements
     end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
