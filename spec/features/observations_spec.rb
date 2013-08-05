b800ce5647499ffed65caac995d963fcce24d075
Simon Walker <s.r.walker101@googlemail.com>
2013-08-05 23:03:41 +0100

    Adds more detail link check

--- spec/features/observations_spec.rb
+++ spec/features/observations_spec.rb
@@ -3,10 +3,19 @@ require "spec_helper"
 describe "observations index" do
     before(:each) do 
         @observation = Observation.create(:observation_number => 10101)
+        visit observations_path
     end
 
     it "should have a list of observations" do
-        visit observations_path
         expect(page).to have_content "10101"
+    end
+
+    it "should show a 'More detail' link" do
+        expect(page).to have_content "More detail"
+    end
+
+    it "should go to the observation detail when 'More detail' clicked" do
+        click_link "More detail"
+        expect(current_path).to eq(observation_path(@observation))
     end
 end
