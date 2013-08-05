66f027f9053c76fdf44d1d684c234f74fb258c81
Simon Walker <s.r.walker101@googlemail.com>
2013-08-05 22:53:38 +0100

    Adds create body

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,4 +1,16 @@
 class ObservationsController < ApplicationController
   def create
+      @observation = Observation.new(observation_params)
+      if @observation.save
+          redirect_to @observation
+      else
+          render :new
+      end
+  end
+
+  private
+
+  def observation_params
+      params.require("observation").permit(:observation_number)
   end
 end
