e1030f43e365e07023a0d12257ae1d83a4a8c974
Simon Walker <s.r.walker101@googlemail.com>
2013-08-05 23:34:58 +0100

    Adds test for measurements list

--- spec/features/measurements_spec.rb
+++ spec/features/measurements_spec.rb
@@ -1,4 +1,14 @@
 require "spec_helper"
 
 describe "measurements index" do
+    before(:each) do
+        @observation = Observation.create(
+            :observation_number => 10101
+        )
+        visit observation_measurements_path(@observation)
+    end
+
+    it "should show the observation number" do
+        expect(page).to have_content "Measurements for observation #{@observation.observation_number}"
+    end
 end
