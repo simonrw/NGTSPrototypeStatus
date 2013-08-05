6f3168ac5f241508de1daec1bb6dea67fe3bce7d
Simon Walker <s.r.walker101@googlemail.com>
2013-08-05 12:26:32 +0100

    Adds spec for no start date

--- spec/models/night_spec.rb
+++ spec/models/night_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Night do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "should accept a valid date" do
+        Night.create(:start_date => 2.days.ago, :end_date => 1.days.ago)
+    end
+
+    it "should fail with out start date specified" do
+        expect { Night.create!(:end_date => 1.days.ago) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 end
