fe55d23e91f2125c9e1a37a9085347afa8b18bff
Simon Walker <s.r.walker101@googlemail.com>
2013-08-05 12:28:37 +0100

    Adds test for invalid start date

--- spec/models/night_spec.rb
+++ spec/models/night_spec.rb
@@ -8,4 +8,8 @@ describe Night do
     it "should fail with out start date specified" do
         expect { Night.create!(:end_date => 1.days.ago) }.to raise_error(ActiveRecord::RecordInvalid)
     end
+
+    it "should fail to save if the start date is outside the range 2011->now" do
+        expect { Night.create!(:start_date => Date.new(2000, 1, 1), :end_date => 2.days.ago) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 end
