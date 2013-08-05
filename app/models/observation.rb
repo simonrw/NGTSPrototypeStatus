39c1ed36548aa4baaeffe14dc270bf74892af878
Simon Walker <s.r.walker101@googlemail.com>
2013-08-05 22:25:38 +0100

    Adds observation annotations

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: observations
+#
+#  id                 :integer          not null, primary key
+#  observation_number :integer
+#  night_id           :integer
+#  log                :text
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class Observation < ActiveRecord::Base
 end
--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: observations
+#
+#  id                 :integer          not null, primary key
+#  observation_number :integer
+#  night_id           :integer
+#  log                :text
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 require 'spec_helper'
 
 describe Observation do
