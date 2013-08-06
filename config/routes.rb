c28ec604736e854bd99da05b34def2d2e1224515
Simon Walker <s.r.walker101@googlemail.com>
2013-08-06 17:55:14 +0100

    Adds root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ NGTSPrototypeStatus::Application.routes.draw do
     resources :observations do
         resources :measurements
     end
+
+    root 'observations#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
