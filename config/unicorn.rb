8dd1e8bb8ffe48963e6587a718f2cd90fcb3f690
Simon Walker <s.r.walker101@googlemail.com>
2013-08-07 21:50:03 +0100

    Enables copy on write

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,8 @@ rails_env = ENV['RAILS_ENV'] || 'production'
 worker_processes (rails_env == 'production' ? 16 : 4)
 
 preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+    GC.copy_on_write_friendly = true
 
 timeout 30
 
