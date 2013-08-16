738771bca012866d117f4f93a19ac2ac5ddf91f7
Simon Walker <s.r.walker101@googlemail.com>
2013-08-16 18:48:13 +0100

    Implements basic http authentication

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+USER_DETAILS = YAML::load_file Rails.root.join('config', 'user_details.yml')
+
 # Initialize the Rails application.
 NGTSPrototypeStatus::Application.initialize!
